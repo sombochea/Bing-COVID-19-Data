6d186dd6bf22f4857e0aff525c8ad56717a73e4d
Sambo Chea <sombochea@cubetiqs.com>
2021-11-26 10:40:11 +0700

    Add header for Cambodia data

--- data/grep-cambodia.sh
+++ data/grep-cambodia.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -e
 
-(cat Bing-COVID19-Data.csv | grep "Cambodia") > Cambodia.csv
+FILENAME="Cambodia.csv"
+echo "ID,Updated,Confirmed,ConfirmedChange,Deaths,DeathsChange,Recovered,RecoveredChange,Latitude,Longitude,ISO2,ISO3,Country_Region,AdminRegion1,AdminRegion2" > ${FILENAME} 
+(cat Bing-COVID19-Data.csv | grep "Cambodia") >> ${FILENAME}
